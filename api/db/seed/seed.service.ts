be76967ad9bd7fe0a291ef721cd1148f17561d62
tam <bwb.alcalajimenez.t@fondespierre.com>
2021-08-31 09:09:05 +0000

    commit pre pull seed correction raph

--- api/db/seed/seed.service.ts
+++ api/db/seed/seed.service.ts
@@ -2,6 +2,12 @@ import * as Faker from 'faker';
 import { Injectable } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { Enterprise } from '../entities/Enterprise.entity';
+import { Status } from 'db/entities/Status.entity';
+import { MeetingType } from 'db/entities/MeetingType.entity';
+import { Meeting } from 'db/entities/Meeting.entity';
+import { Admin } from 'db/entities/Admin.entity';
+import { User } from 'db/entities/User.entity';
+import { Provider } from 'db/entities/Provider.entity';
 
 @Injectable()
 export class SeedService {
@@ -31,6 +37,61 @@ export class SeedService {
       });
 
       Enterprise.find().then((e) => console.log(e));
+
+      loop(3)(() => {
+        let status = new Status();
+        status.enterprise = Enterprise.id();
+        status.meetingType = ;
+        status.majorityMin = Faker.datatype.number();
+        status.majorityMax = Faker.datatype.number();
+        status.quorumMin = Faker.datatype.number();
+        status.quorumMax = Faker.datatype.number();
+        status.power = Faker.datatype.number();
+        status.save();
+      });
+
+      loop(1)(()=>{
+        let meetingType = new MeetingType();
+        meetingType.name = ;
+        meetingType.save();
+      });
+
+      loop(1)(()=>{
+        let meeting = new Meeting;
+        meeting.date = Faker.datatype.datetime();
+        meeting.location = Faker.address.streetAddress() + ' ' + Faker.address.city();
+        meeting.enterprise = ;
+        meeting.meetingType = ;
+        meeting.state = Faker.datatype.boolean();
+        meeting.save();
+      });
+
+      loop(1)(()=>{
+        let admin = new Admin();
+        admin.user = ;
+        admin.password = Faker.internet.password();
+        admin.save();
+      });
+
+      loop(10)(()=>{
+        let user = new User();
+        user.firstName = Faker.name.firstName();
+        user.lastName = Faker.name.lastName();
+        user.email = Faker.internet.email();
+        user.enterprise = ;
+        user.save();
+      });
+
+      loop(1)(()=>{
+        let provider = new Provider();
+        provider.name = Faker.internet.;
+        provider.save();
+      });
+
+
+
+
+
 
       /**
        * SEED END

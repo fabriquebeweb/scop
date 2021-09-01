85ba4ca02ae3cc3b043906797d715874b9984781
tam <bwb.alcalajimenez.t@fondespierre.com>
2021-09-01 22:33:36 +0000

    /admin/meetings Fixes #18 meeting-detail window

--- client/src/app/admin-meetings/admin-meeting-detail/admin-meeting-detail.component.ts
+++ client/src/app/admin-meetings/admin-meeting-detail/admin-meeting-detail.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-meeting-detail',
   templateUrl: './admin-meeting-detail.component.html',
-  styleUrls: ['./admin-meeting-detail.component.css']
+  styleUrls: ['./admin-meeting-detail.component.css'],
 })
 export class AdminMeetingDetailComponent implements OnInit {
+  @Input() meetingId = '';
+  @Input() meetingTitle = '';
+  @Input() meetingDate = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

98ef29e70916089ad0f996dbfc751569116f9d16
atlasrw <atlasrw@icloud.com>
2021-09-02 08:43:17 +0000

    Meeting Module bootstrap

--- client/src/app/meeting/meeting.module.tns.ts
+++ client/src/app/meeting/meeting.module.tns.ts
@@ -1,12 +1,15 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from '@nativescript/angular';
 import { MeetingComponent } from '@src/app/meeting/meeting.component';
+import { MeetingRoutingModule } from './meeting.routing.tns';
 
 @NgModule({
   declarations: [
     MeetingComponent
   ],
   imports: [
+    NativeScriptCommonModule,
+    MeetingRoutingModule
   ],
   schemas: [
     NO_ERRORS_SCHEMA
--- client/src/app/meeting/meeting.module.ts
+++ client/src/app/meeting/meeting.module.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MeetingComponent } from '@src/app/meeting/meeting.component';
+import { MeetingRoutingModule } from './meeting.routing';
 
 @NgModule({
   declarations: [
     MeetingComponent
   ],
   imports: [
+    CommonModule,
+    MeetingRoutingModule
   ]
 })
 export class MeetingModule { }

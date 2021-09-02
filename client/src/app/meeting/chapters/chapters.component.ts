4507a2c22df8f5b1cc73156e9c132e3fb25cfc8a
Nyviii <bwb.georges.l@fondespierre.com>
2021-09-02 10:50:22 +0000

    generate components

---
+++ client/src/app/meeting/chapters/chapters.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'meeting-chapters',
+  templateUrl: './chapters.component.html'
+})
+export class MeetingChaptersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

39e80e8e92edd47ec8deaab512cf79c287ec976c
tam <bwb.alcalajimenez.t@fondespierre.com>
2021-09-02 11:07:59 +0000

    scroll bar sidebar done

--- client/src/app/admin/meetings/meetings.component.ts
+++ client/src/app/admin/meetings/meetings.component.ts
@@ -3,7 +3,7 @@ import { Meeting } from '@src/app/misc/entities/Meeting'
 
 @Component({
   selector: 'admin-meetings',
-  templateUrl: './meetings.component.html'
+  templateUrl: './meetings.component.html',
 })
 export class AdminMeetingsComponent implements OnInit {
   meetings: Meeting[] = [
@@ -11,53 +11,69 @@ export class AdminMeetingsComponent implements OnInit {
       id: 1,
       meetingType: {
         id: 1,
-        title: 'Assemblée Générale Ordinaire'
+        title: 'Assemblée Générale Ordinaire',
       },
-      date: new Date('2021-01-12')
+      date: new Date('2021-01-12'),
     },
     {
       id: 2,
       meetingType: {
         id: 2,
-        title: 'Assemblée Générale Extraordinaire'
+        title: 'Assemblée Générale Extraordinaire',
       },
-      date: new Date('2021-06-12')
+      date: new Date('2021-06-12'),
     },
     {
       id: 3,
       meetingType: {
         id: 3,
-        title: 'Réunion'
+        title: 'Réunion',
       },
-      date: new Date('2021-06-17')
+      date: new Date('2021-06-17'),
     },
     {
       id: 4,
       meetingType: {
         id: 4,
-        title: 'Conseil d\'Administration'
+        title: "Conseil d'Administration",
       },
-      date: new Date('2021-08-22')
+      date: new Date('2021-08-22'),
     },
     {
       id: 5,
       meetingType: {
         id: 3,
-        title: 'Réunion'
+        title: 'Réunion',
       },
-      date: new Date('2021-09-01')
+      date: new Date('2021-09-01'),
     },
     {
       id: 6,
       meetingType: {
         id: 3,
-        title: 'Réunion'
+        title: 'Réunion',
       },
-      date: new Date('2021-09-01')
-    }
-  ]
+      date: new Date('2021-09-01'),
+    },
+    {
+      id: 7,
+      meetingType: {
+        id: 3,
+        title: 'Réunion',
+      },
+      date: new Date('2021-09-01'),
+    },
+    {
+      id: 8,
+      meetingType: {
+        id: 3,
+        title: 'Réunion',
+      },
+      date: new Date('2021-09-01'),
+    },
+  ];
 
   constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 }

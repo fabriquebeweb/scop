8b349e782d498a96c89888aea601e6e8be9e332c
tam <bwb.alcalajimenez.t@fondespierre.com>
2021-09-02 12:56:27 +0000

    maj nom component result et nettoyage fichiers

---
+++ client/src/app/meeting/chapters/result/result.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'chapter-result',
+  templateUrl: './result.component.html'
+})
+export class MeetingChaptersResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/meeting/meeting.module.ts
+++ client/src/app/meeting/meeting.module.ts
@@ -5,17 +5,16 @@ import { MeetingRoutingModule } from '@src/app/meeting/meeting.routing';
 import { MeetingDocumentsComponent } from '@src/app/meeting/documents/documents.component';
 import { MeetingChaptersComponent } from '@src/app/meeting/chapters/chapters.component';
 import { MeetingPresenceComponent } from '@src/app/meeting/presence/presence.component';
+import { MeetingChaptersResultComponent } from '@src/app/meeting/chapters/result/result.component';
 
 @NgModule({
   declarations: [
     MeetingComponent,
     MeetingDocumentsComponent,
     MeetingChaptersComponent,
-    MeetingPresenceComponent
+    MeetingPresenceComponent,
+    MeetingChaptersResultComponent
   ],
-  imports: [
-    CommonModule,
-    MeetingRoutingModule
-  ]
+  imports: [CommonModule, MeetingRoutingModule],
 })
-export class MeetingModule { }
+export class MeetingModule {}

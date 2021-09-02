332772a3b1b252d9428510cfbb764bd2b1ca5712
atlasrw <atlasrw@icloud.com>
2021-09-02 11:34:08 +0000

    API Routes

--- api/src/admin/admin.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@nestjs/core'
-import { AdminMeetingsModule } from './meetings/meetings.module'
-
-export const routes: Routes = [
-  {
-    path: 'meetings',
-    module: AdminMeetingsModule
-  }
-]

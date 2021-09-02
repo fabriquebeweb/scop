1b611bcdedcc712573ce4a77166a2fb86e9f34cc
atlasrw <atlasrw@icloud.com>
2021-09-02 21:17:20 +0000

    API Start

--- api/src/admin/meetings/meetings.controller.ts
+++ api/src/admin/meetings/meetings.controller.ts
@@ -1,11 +1,33 @@
-import { Controller, Get } from '@nestjs/common'
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common'
+import { Meeting } from 'db/entities/Meeting.entity'
+import { AdminMeetingsService } from './meetings.service'
 
-@Controller('meetings')
+@Controller('/meetings')
 export class AdminMeetingsController {
 
+  constructor(
+    private readonly meetingsService: AdminMeetingsService
+  ) {}
+
   @Get()
-  adminMeeting() {
-    return "ADMIN MEETING"
+  meetingList() {
+    return this.meetingsService.getAllMeetings()
+  }
+
+  @Get('/:meeting')
+  meetingDetails(@Param('meeting') meetingId: number) {
+    return this.meetingsService.getOneMeeting(meetingId)
+  }
+
+  @Post()
+  newMeeting(@Body() meeting: Meeting) {
+    this.meetingsService.saveOneMeeting(meeting)
+  }
+
+  @Get('/test')
+  meetingTest() {
+    this.meetingsService.test()
+    return "DONE"
   }
 
 }

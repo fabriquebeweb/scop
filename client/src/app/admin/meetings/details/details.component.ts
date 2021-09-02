16882f2c5955c577de0474dbac853c0d7b3a46c8
atlasrw <atlasrw@icloud.com>
2021-09-02 06:28:12 +0000

    Meeting Details Fix

--- client/src/app/admin/meetings/details/details.component.ts
+++ client/src/app/admin/meetings/details/details.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { Meeting } from '@src/app/misc/entities/Meeting'
 
 @Component({
   selector: 'meeting-details',
   templateUrl: './details.component.html'
 })
 export class AdminMeetingDetailsComponent implements OnInit {
+  @Input() meeting: Meeting
 
   constructor() { }
 

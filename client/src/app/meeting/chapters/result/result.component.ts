63c29fb93b62ea114daceb04470123480cbe2b8b
tam <bwb.alcalajimenez.t@fondespierre.com>
2021-09-03 12:56:28 +0000

    add bar chart and result

--- client/src/app/meeting/chapters/result/result.component.ts
+++ client/src/app/meeting/chapters/result/result.component.ts
@@ -16,33 +16,52 @@ export class MeetingChaptersResultComponent implements OnInit {
     const canvas = <HTMLCanvasElement>document.getElementById('myChart');
     const ctx = canvas.getContext('2d');
     var myChart = new Chart(ctx, {
-      type: 'bar',
+      type: 'horizontalBar',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Pour', 'Contre', 'Abstention'],
         datasets: [
           {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Nombre de votes',
+            barPercentage: 0.5,
+            categoryPercentage: 1.0,
+            data: [10, 5, 3],
             backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
+              '#36A2EB80',
+              '#FF634F80',
+              '#FFCE6080'
+            ]
           },
         ],
+        
       },
+      options: {
+        responsive : false,
+        legend:
+        {
+          display: false
+        },
+        scales: {
+            xAxes: [{
+              ticks: {
+                    min: 0, // Edit the value according to what you need
+                    display: false
+                },
+              gridLines: {
+                  display: false,
+              }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true,
+                    
+                },
+                gridLines: {
+                display: false,
+            }
+            }]
+        }
+          
+      }
       
     });
     

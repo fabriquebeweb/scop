6eb63a9a3edf0f22a33dc7e0dc294111d6de8a2a
Nyviii <bwb.georges.l@fondespierre.com>
2021-09-06 13:17:04 +0000

    /meeting [chapters] Fixes #22

--- client/src/app/meeting/chapters/details/details.component.ts
+++ client/src/app/meeting/chapters/details/details.component.ts
@@ -10,7 +10,7 @@ export class MeetingChaptersDetailsComponent implements OnInit {
     {
     id: 1,
     title: "CHANGEMENT D'HOMME DE MÉNAGE",
-    description: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum",
+    description: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum",
     summary: "summary lalala",
     question: "on l'enterre où ?"
   }

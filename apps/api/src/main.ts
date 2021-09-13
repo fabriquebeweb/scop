f042b61ab9f76a79766e78ccdc4db299346f1c80
atlasrw <atlasrw@icloud.com>
2021-09-13 11:12:16 +0000

    fox port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
   // const port = process.env.PORT || 3333
-  await app.listen(process.env.PORT || 3333)
+  await app.listen(process.env.PORT || 3000)
 }
 
 bootstrap()

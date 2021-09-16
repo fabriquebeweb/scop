dd810f8466530174876c9e7e369ebd965a98ff11
sidney_carlos <bwb.carlos.s@fondespierre.com>
2021-09-16 23:02:46 +0000

    began observables pattern

--- apps/web/src/app/admin/members/details/details.component.ts
+++ apps/web/src/app/admin/members/details/details.component.ts
@@ -1,14 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '@scop/interfaces';
+import { Subscription } from 'rxjs';
+import { AdminMembersService } from '../members.service';
 
 @Component({
   selector: 'member-details',
   templateUrl: './details.component.html',
 })
-export class AdminMembersDetailsComponent implements OnInit {
+export class AdminMembersDetailsComponent implements OnInit, OnDestroy {
 
-  constructor(){}
+  @Input() member!: User
+  observer!: Subscription
+
+  constructor(
+    public readonly service: AdminMembersService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router
+  ){}
 
   ngOnInit() : void
-  {}
+  {
+    this.observer = this.route.params.subscribe(params => this.refreshMember(params.members)) //ou member?
+  }
+
+  ngOnDestroy() : void
+  {
+    this.observer.unsubscribe
+  }
+
+  //récupère l'id du member, affecte à la variable membre l'id courant
+  refreshMember(id: number) : void
+  {
+    this.service.getMember(id)
+    .then(member => this.setMember(member))
+    .catch(() => this.onError())
+  }
+
+  setMember(member: User) : void
+  {
+    this.member = member
+  }
+
+  onError() : void
+  {
+    this.router.navigateByUrl('/admin/members/error')
+  }
 
 }

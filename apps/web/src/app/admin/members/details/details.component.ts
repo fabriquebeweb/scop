bd910a0fc52cfbabee001f919a69fd5b858e2bb3
sidney_carlos <bwb.carlos.s@fondespierre.com>
2021-09-20 09:12:39 +0000

    Comment details

--- apps/web/src/app/admin/members/details/details.component.ts
+++ apps/web/src/app/admin/members/details/details.component.ts
@@ -27,30 +27,56 @@ export class AdminMembersDetailsComponent implements OnInit, OnDestroy {
     this.observer.unsubscribe
   }
 
-  //récupère l'id du member, affecte à la variable membre l'id courant
+  /** Mise à jour d'un membre selon son ID
+   * Récupération d'un membre : utilisation de la méthode getMember
+   * Si récup ok alors on appelle la méthode setmember
+   * Sinon on appelle la methode onError
+   * @param id 
+   */
   refreshMember(id: number): void {
     this.service.getMember(id)
       .then(member => this.setMember(member))
       .catch(() => this.onError())
   }
 
+  /**
+   * Affectation du member
+   * @param member 
+   */
   setMember(member: User): void {
     this.member = member
   }
 
+  /**
+   * Redirection vers la page erreur du router
+   */
   onError(): void {
     this.router.navigateByUrl('/admin/error')
   }
 
+  /** Mise à jour de la vue
+   * Appel de la méthode put de member.service (mise à jour en bdd)
+   * Puis mise à jour de la liste des membres dans la vue
+   */
   updateMember() {
     this.service.resetMember(this.member)
       .then(modifiedMember => this.updateMemberList(modifiedMember))
   }
 
+  /**
+   * Mise à jour de la liste des membres: à l'index du member réaffecter par les nouvelles informations entrées par l'utilisateur
+   * @param modifiedMember 
+   */
   updateMemberList(modifiedMember: User): void {
     this.service.members[this.index(modifiedMember)] = modifiedMember
   }
 
+  /**
+   * Récupérer l'index du member courant dans la liste members (service)
+   * Chercher la correspondance entre l'index de l'id la liste et l'id du member courant
+   * @param member 
+   * @returns l'index de correspondance
+   */
   index(member: User): number {
     return this.service.members.findIndex(obj => obj.id == member.id)
   }

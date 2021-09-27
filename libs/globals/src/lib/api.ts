6000ed35370e0db2e4a9363e7a0f7ff8ecca34e7
atlasrw <atlasrw@icloud.com>
2021-09-27 18:02:33 +0000

    API FIX

--- libs/globals/src/lib/api.ts
+++ libs/globals/src/lib/api.ts
@@ -3,8 +3,8 @@ import { NestApplicationOptions } from '@nestjs/common'
 
 export class API {
 
-  // static HOST: string = 'vps-6cf378a8.vps.ovh.net'
-  static HOST: string = 'localhost'
+  static HOST: string = 'vps-6cf378a8.vps.ovh.net'
+  // static HOST: string = 'localhost'
   static PORT: number = 3000
   static PREFIX: string = '/api'
   static PATH: string = `http://${API.HOST}:${API.PORT}${API.PREFIX}`
